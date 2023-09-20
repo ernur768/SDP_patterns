16840265ef392897912b54abb7b4bb832000275a
Yernur <220776@astanait.edu.kz>
2023-09-20 17:22:56 +0600

    decoratorPattern

---
+++ decoratorPattern/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+// Component interface ---------------------
+
+type Player interface {
+	Play() string
+}
+
+// Concrete component -----------------
+
+type NewPlayer struct {
+}
+
+func (n NewPlayer) Play() string {
+	return "enjoying the game."
+}
+
+// Concrete decorators
+
+type Tank struct {
+	Player Player
+}
+
+func (t Tank) Play() string {
+	return t.Player.Play() + " Keeping the boss`s agro."
+}
+
+type DamageDealer struct {
+	Player Player
+}
+
+func (d DamageDealer) Play() string {
+	return d.Player.Play() + " Dealing high damage."
+}
+
+type Healer struct {
+	Player Player
+}
+
+func (h Healer) Play() string {
+	return h.Player.Play() + " Healing teammates"
+}
+
+// client ----------------------------
+
+func main() {
+
+	noob := NewPlayer{}
+	fmt.Printf("%s\n\n", noob.Play())
+
+	pro := Tank{Player: noob}
+	fmt.Printf("%s\n\n", pro.Play())
+
+	hacker := DamageDealer{Player: pro}
+	fmt.Printf("%s\n\n", hacker.Play())
+
+	god := Healer{Player: hacker}
+	fmt.Printf("%s\n\n", god.Play())
+}
